1872566b222a668cca8217d769e0867c0cfe12bc
Tushar <tushar.futball@gmail.com>
2025-06-16 15:14:16 +0100

    Initial backend commit

---
+++ src/main/java/com/tushar/smartmark/SmartMarkApplication.java
@@ -0,0 +1,13 @@
+package com.tushar.smartmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tushar/smartmark/SmartMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.smartmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
